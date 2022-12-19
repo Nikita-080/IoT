674315d40e1854a35c40851815a85c9214e7ae4b
AzmagulovA <74853801+AzmagulovA@users.noreply.github.com>
2022-12-19 16:18:26 +0500

    Создание тестового варианта сервера

--- server/server.py
+++ server/server.py
@@ -1,1 +1,36 @@
-test commi
+from http.server import HTTPServer, BaseHTTPRequestHandler #класс
+
+from io import BytesIO #класс
+
+import random#класс 
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    # определяем метод `do_GET` 
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        s=random.choice(["no","18.12.2022 14:30"])
+        print(s)
+        self.wfile.write(str.encode(s))
+
+    # определяем метод `do_POST` 
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        response = BytesIO()
+        response.write(b'This is POST request. ')
+        response.write(b'Received: ')
+        response.write(body)
+        self.wfile.write(response.getvalue())
+
+        print(body)
+
+httpd = HTTPServer(('', 8080), SimpleHTTPRequestHandler)
+httpd.serve_forever()
