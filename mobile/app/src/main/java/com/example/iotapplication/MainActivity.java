9297457770f7ee156bc4992e032c7758bc311597
Никита <nikitarabov162@gmail.com>
2022-12-21 18:34:23 +0500

    updating class structure

--- mobile/app/src/main/java/com/example/iotapplication/MainActivity.java
+++ mobile/app/src/main/java/com/example/iotapplication/MainActivity.java
@@ -20,16 +20,19 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    int id=100;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv=findViewById(R.id.textView);
+        tv=findViewById(R.id.textView); //test ui allow
 
         createNotificationChannel();
-        //RequestSender RC = new RequestSender();
-        //RC.execute(this);
+
+        RequestSender RC = new RequestSender();
+        RC.execute(this);
     }
     private void createNotificationChannel() {
 
@@ -49,14 +52,15 @@ public class MainActivity extends AppCompatActivity {
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "101")
                 .setSmallIcon(R.drawable.warningicon)
-                .setContentTitle("ВНИМАНИЕ")
-                .setContentText("сработала сигнализация ")
+                .setContentTitle("TestNot")
+                .setContentText("Hellow from Notification")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
 
-
+        id+=1;
+        if (id>200) id=101;
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
-        notificationManager.notify(101, builder.build());
+        notificationManager.notify(id, builder.build());
          /*
         Toast toast = Toast.makeText(getApplicationContext(),
                 "Пора покормить кота!", Toast.LENGTH_SHORT);
@@ -64,6 +68,78 @@ public class MainActivity extends AppCompatActivity {
 
           */
 
+    }
+
+    class RequestSender extends AsyncTask<MainActivity, String, Void> {
+        @Override
+        protected Void doInBackground(MainActivity... wins)
+        {
+            while (true) {
+                String data = "";
+                try {
+                    URL url = new URL("http://192.168.0.100:8080");
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("GET");
+                    con.setRequestProperty("Content-Type", "text/html");
+                    con.setConnectTimeout(1500);
+                    con.setReadTimeout(1500);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String inputLine;
+                    while ((inputLine = in.readLine()) != null) {
+                        data += inputLine;
+                    }
+                    in.close();
+                } catch (Exception e) {
+                    data = "error " + e.toString();
+                }
+                publishProgress(data);
+
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException e) {
+                /*
+                new AlertDialog.Builder(window)
+                        .setTitle("Time Error")
+                        .setMessage(e.toString())
+                        .show();
+
+                 */
+                }
+            }
+
+        }
+        @Override
+        protected void onProgressUpdate(String... messages) {
+            super.onProgressUpdate(messages[0]);
+
+            //TextView tv=findViewById(R.id.textView);
+
+            if (messages[0].length() >= 5 && messages[0].substring(0, 5).equals("error")) {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "101")
+                        .setSmallIcon(R.drawable.warningicon)
+                        .setContentTitle("IoTapplication")
+                        .setContentText(messages[0])
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+                notificationManager.notify(100, builder.build());
+            }
+            else if (!messages[0].equals("no")) {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "101")
+                        .setSmallIcon(R.drawable.signalicon)
+                        .setContentTitle("ВНИМАНИЕ")
+                        .setContentText("сработала сигнализация")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                id+=1;
+                if (id>200) id=101;
+
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+                notificationManager.notify(id, builder.build());
+
+                tv.setText(messages[0]);
+            }
+        }
     }
 }
 /*
@@ -128,64 +204,5 @@ class Box
     public Box(MainActivity win, String data) {
         window=win;
         answer=data;
-    }
-}
-class RequestSender extends AsyncTask<MainActivity, Box, Void> {
-    @Override
-    protected Void doInBackground(MainActivity... wins)
-    {
-        while (true) {
-            String data = "";
-            try {
-                URL url = new URL("http://192.168.0.100:8080");
-                HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                con.setRequestMethod("GET");
-                con.setRequestProperty("Content-Type", "text/html");
-                con.setConnectTimeout(1500);
-                con.setReadTimeout(1500);
-                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                String inputLine;
-                while ((inputLine = in.readLine()) != null) {
-                    data += inputLine;
-                }
-                in.close();
-            } catch (Exception e) {
-                data = "error\n" + e.toString();
-            }
-            publishProgress(new Box(wins[0],data));
-
-            try {
-                TimeUnit.SECONDS.sleep(5);
-            } catch (InterruptedException e) {
-                /*
-                new AlertDialog.Builder(window)
-                        .setTitle("Time Error")
-                        .setMessage(e.toString())
-                        .show();
-
-                 */
-            }
-        }
-
-    }
-    @Override
-    protected void onProgressUpdate(Box... boxes) {
-        super.onProgressUpdate();
-        if (boxes[0].answer!="no")
-        {
-
-        }
-
-        if (boxes[0].answer.length() >= 5 && boxes[0].answer.substring(0, 5) == "error") {
-            new AlertDialog.Builder(boxes[0].window)
-                    .setTitle("Error")
-                    .setMessage(boxes[0].answer)
-                    .show();
-        } else if (boxes[0].answer != "no") {
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(boxes[0].window, "channelID")
-                    .setContentTitle("ВНИМАНИЕ")
-                    .setContentText("сработала сигнализация " + boxes[0].answer)
-                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-        }
     }
 }
