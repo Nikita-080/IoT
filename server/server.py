2f42e979183149b344d83ec447d49981f7104c12
AzmagulovA <74853801+AzmagulovA@users.noreply.github.com>
2022-12-24 11:35:40 +0500

    РедактированиеОтправкиЗапросаОтКонтроллераКПользователю

--- server/server.py
+++ server/server.py
@@ -1,27 +1,28 @@
-from datetime import datetime
 from http.server import HTTPServer, BaseHTTPRequestHandler #класс
 
 from io import BytesIO #класс
 
-
+toclientmessage=''
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    toclientmessage=''
+    
     
     # определяем метод `do_GET` 
     def do_GET(self):
+        global toclientmessage
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
-        self.end_headers
+        self.end_headers()
         print(toclientmessage)
         if(toclientmessage==''):
-            self.wfile.write(str.encode('no'))
+            self.wfile.write(str.encode("empty"))
         else:
-            self.wfile.write(str.encode(toclientmessage))
+            self.wfile.write(str.encode("w"))
             toclientmessage=''
         
 
     # определяем метод `do_POST` 
     def do_POST(self):
+        global toclientmessage
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
         self.send_response(200)
@@ -32,9 +33,9 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         response.write(b'Received: ')
         response.write(body)
         self.wfile.write(response.getvalue())
+        toclientmessage="notnull"
 
-        toclientmessage=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        
+
         print(body)
 
 httpd = HTTPServer(('', 8080), SimpleHTTPRequestHandler)
