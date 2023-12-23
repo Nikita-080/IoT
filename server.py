14339dae12fd130d0953d13040ed32e3154dbe29
AzmagulovA <74853801+AzmagulovA@users.noreply.github.com>
2023-12-23 15:49:56 +0500

    Add files via upload
    Изменен метод получения ip адреса и реагирование на отправителя запроса

--- server.py
+++ server.py
@@ -2,13 +2,9 @@ from http.server import HTTPServer, BaseHTTPRequestHandler #класс
 import qrcode
 import random
 import socket
-from io import BytesIO #класс
 
-toclientmessage=''
-def get_local_ip():
-    hostname = socket.gethostname()
-    local_ip = socket.gethostbyname(hostname)
-    return local_ip
+
+from io import BytesIO #класс
 
 def random_token():
     str1 = '123456789'
@@ -17,50 +13,82 @@ def random_token():
     str4 = str1+str2+str3
     ls = list(str4)
     random.shuffle(ls)
-    psw = ''.join([random.choice(ls) for x in range(12)])
+    psw = ''.join([random.choice(ls) for x in range(4)])
     return psw
+toclientmessage=''
+frommobile=''#Embedded
+token=str(random_token())
 
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]   
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     
     
     # определяем метод `do_GET` 
     def do_GET(self):
+        print("receive get")
         global toclientmessage
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        
-        print(toclientmessage)
-        if(toclientmessage==''):
-            self.wfile.write(str.encode("empty"))
+        User_Agent = str(self.headers['User-Agent'])
+        User_Token = str(self.headers['token'])
+        if(User_Agent=='mobile'):
+            if(token==User_Token):  
+                print(toclientmessage)
+                if(toclientmessage==''):
+                    self.wfile.write(str.encode("empty"))
+                else:
+                    self.wfile.write(str.encode("w"))
+                    toclientmessage=''
+            else:
+                self.send_response(403)
+                self.end_headers()
         else:
-            self.wfile.write(str.encode("w"))
-            toclientmessage=''
+            self.wfile.write(str.encode(frommobile))
         
 
     # определяем метод `do_POST` 
     def do_POST(self):
+        print("receive post")
         global toclientmessage
         content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        response = BytesIO()
-        response.write(b'This is POST request. ')
-        response.write(b'Received: ')
-        response.write(body)
-        self.wfile.write(response.getvalue())
-
-        toclientmessage="notnull"
-        print(body)
+        User_Agent = str(self.headers['User-Agent'])
+        User_Token = str(self.headers['token'])
+        
+        if(User_Agent=='mobile'):
+            if(token==User_Token):
+                frommobile = self.rfile.read(content_length)
+                self.send_response(200)
+                self.end_headers()
+            else:
+                self.send_response(403)
+                self.end_headers()
+        elif(User_Agent=='Embedded'):            
+            body = self.rfile.read(content_length)
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            response = BytesIO()
+            response.write(b'This is POST request. ')
+            response.write(b'Received: ')
+            response.write(body)
+            self.wfile.write(response.getvalue())
+            toclientmessage="notnull"
+            print(body)
+        else:
+            self.send_response(400)
+            self.end_headers()
 
     
   
-data = "https://" + str(get_local_ip()) + ":8080-"+str(random_token())
+data = "http://" + str(get_local_ip()) + ":8080-"+token
 filename = "site.png"
 img = qrcode.make(data)
 img.save(filename)
 print(get_local_ip())
+print(token)
 httpd = HTTPServer(('', 8080), SimpleHTTPRequestHandler)
 httpd.serve_forever()
